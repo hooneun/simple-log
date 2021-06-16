eef88a8611f07718705ca5ce50180117da3d928a
kimhoon <kimhoon0316@gmail.com>
2021-06-16 18:48:05 +0900

    dev.ing config

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,44 @@
+#[derive(Debug, PartialEq)]
+enum Level {
+    Emergency,
+    Alert,
+    Critical,
+    Error,
+    Warning,
+    Notice,
+    Informational,
+    Debug,
+}
+
+#[derive(Debug, PartialEq)]
+enum Kind {
+    File,
+    //    Telegram,
+    //    Slack,
+    //    Email
+}
+
+#[derive(Debug)]
+pub struct Config {
+    level: Level,
+    kind: Kind,
+}
+
+pub struct Logger {}
+
+impl Logger {}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn config_setting() {
+        let config = Config {
+            level: Level::Emergency,
+            kind: Kind::File,
+        };
+
+        assert_eq!(Level::Emergency, config.level);
+        assert_eq!(Kind::File, config.kind);
     }
 }
